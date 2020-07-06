4644c64214e2c8d239d6f05fcbec905c6efa9bbf
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-07-06 20:58:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,19 +2,19 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  configure do
-    enable :sessions
-    set :session_secret
-  end
+  # configure do
+  #   enable :sessions
+  #   set :session_secret
+  # end
   
   get '/' do
     erb :index
   end
   
   post '/checkout' do
-    session["item"] = params[:item]
-    @session = session
-     
+    # session["item"] = params[:item]
+    # @session = session
+     params[:item]
      
   end
   
